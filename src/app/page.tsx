46a2e7aa7b6513b93afdc4b4c6c2134cd8cd455c
hema <bhemavallika@gmail.com>
2025-07-04 20:48:55 +0530

    Add demo, how it works, and pricing sections to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 import BgGradient from "@/components/common/bgGradient";
 import HeroSection from "@/components/home/heroSection";
-
+import DemoSection from "@/components/home/demoSection";
+import HowItWorksSection from "@/components/home/howItWorks";
+import PricingSection from "@/components/home/pricingSection";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="relative w-full">
+    <div className="relative w-full ">
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
-      </div>
+        <DemoSection />
+      <HowItWorksSection />
+      <PricingSection />
 
-      {/* <DemoSection /> */}
-      {/* HowItWorksSection */}
-      {/* <PricingSection /> */}
+
+      </div>
+      {/* <Button>hema vallika btn</Button> */}
+
+      
       {/* <CTASection /> */}
     </div>
   );
---
+++ src/components/home/pricingSection.tsx
@@ -0,0 +1,109 @@
+import Link from "next/link"; // Make sure you're using Next.js and this is imported
+import { cn } from "@/lib/utils"; // Adjust the import based on your project
+import { ArrowRight, CheckIcon } from "lucide-react";
+
+
+type PriceType ={
+    name:string;
+    price:number;
+    description:string;
+    items:string[];
+    id:string;
+    paymentLink:string;
+    priceId:string;
+}
+
+const plans = [
+    {
+        id:"basic",
+        name:'Baisc',
+        price:9,
+        description:'For Professionals and teams',
+        items: [
+            'unlimited PDF Summaries',
+            'Priority Processing',
+            'E-mail Support',
+            
+        ],
+        paymentLink:"",
+        priceId:""
+
+
+    },
+    {
+        id:'pro',
+        name:'Pro',
+        price:19,
+        description:'For Professionals and teams',
+        items: [
+            'unlimited PDF Summaries',
+            'Priority Processing',
+            '24/7 priority Support',
+            'Markdown Export'
+        ],
+        paymentLink:"",
+        priceId:""
+
+    }
+]
+
+const Pricingcard = ({ name, price, description, items, id, paymentLink }: PriceType) => {
+  return (
+    <div className="relative w-full max-w-lg">
+      <div
+        className={cn(
+          "relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 border-[1px] border-gray-500/20 rounded-2xl",
+          id === "pro" && "border-rose-500 gap-5 border-2"
+        )}
+      >
+        <div className="flex justify-between items-center gap-4">
+          <div>
+            <p className="text-lg lg:text-xl font-bold capitalize">{name}</p>
+            <p className="text-base-content/80 mt-2">{description}</p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <p className="text-5xl tracking-tight font-extrabold
+          ">${price}</p>
+          <div className="flex flex-col justify-end mb-[4px]">
+            <p className="text-xs uppercase font-semibold">USD</p>
+            <p className="text-xs">/month</p>
+          </div>
+        </div>
+        <div className="space-y-2.5 leading-relaxed text-base flex-1">
+          {items.map((item, idx) => (
+            <li key={idx} className="flex items-center gap-2">
+                <CheckIcon size={18}/>
+                <span>{item}</span>
+            </li>
+          ))}
+        </div>
+        <div className="space-y-2 flex justify-center w-full">
+          <Link
+            href={paymentLink}
+            className={cn("w-full rounded-full flex items-center justify-center gap-2 bg-linear-to-r from-rose-800 to-rose-500 hover:from-rose-500 hover:to-rose-800 text-2hite border-2 py-2", id=== 'pro'?'border-rose-900':'border-rose-100 from-rose-400 to-rose-500')}
+          >
+            Buy Now <ArrowRight size={10}></ArrowRight>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function PricingSection() {
+  return (
+    <section className="relative overflow-hidden max-w-lg hover:scale-105 hover:transition-all duration-300">
+      <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
+        <div className="flex items-center justify-center w-full pb-12">
+          <h2 className="uppercase font-bold text-xl mb-8 text-red-500">Pricing</h2>
+        </div>
+        <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8 ">
+          {plans.map((plan) => (
+            <Pricingcard key={plan.id} {...plan} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
